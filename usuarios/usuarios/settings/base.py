88ce7712a7f74782c8fd608c230a2a7b375b56e9
foint4it <walter.delafuente@gmail.com>
2022-02-20 16:34:36 -0300

    Proyecto terminado

--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -36,7 +36,7 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = ('applications.users',)
 
 THIRD_PARTY_APPS = ()
 
@@ -91,6 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
